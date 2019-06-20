8df14d1876b3d1b8bdcd1ef4920ead787c1d4b16
Diljot Singh <diljots99@gmail.com>
2019-06-20 16:38:42 +0530

    Backpresed issue resolved

--- app/src/main/java/com/fragmentdemo/MainActivity.java
+++ app/src/main/java/com/fragmentdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.fragmentdemo;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.View;
@@ -10,27 +11,27 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Fragment fragment;
-
+    int count = 0;
+    int count1 = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void myclick(View view) {
+    public void myClick(View view) {
         Fragment fragment = new Myfragment();
         getSupportFragmentManager().beginTransaction().replace(R.id.fragmentview, fragment).addToBackStack("").commit();
-        if (!(fragment instanceof onBackpressd) || !((IOnBackPressed) fragment).onBackPressed()) {
-            super.onBackPressed();
-        }
+        count++;
+
 
     }
 
 
-    public void myclick1(View view) {
+    public void myClick1(View view) {
         Fragment fragment = new Myfragment1();
         getSupportFragmentManager().beginTransaction().replace(R.id.fragmentview1, fragment).addToBackStack("").commit();
-
+        count1++;
     }
 
     public void myappclick(View view) {
@@ -40,22 +41,22 @@ public class MainActivity extends AppCompatActivity {
     public void myappclick1(View view) {
         Toast.makeText(MainActivity.this, "this is my app fragment 2", Toast.LENGTH_LONG).show();
     }
-/*
+
 
     @Override
     public void onBackPressed() {
 
-        int fragment = getSupportFragmentManager().getBackStackEntryCount();
+        int i = getSupportFragmentManager().getBackStackEntryCount();
 
-        if (fragment == 0) {
+        if (i==0) {
             super.onBackPressed();
-            finish();
+
            }
         else {
-            getSupportFragmentManager().popBackStack();
+            getSupportFragmentManager().popBackStack(null , FragmentManager.POP_BACK_STACK_INCLUSIVE);
         }
 
     }
-*/
+
 }
 
